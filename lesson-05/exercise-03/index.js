917d4e973f262b57deea6f1c17548ede9de59d85
pyfkin <pyfkin@gmail.com>
2019-01-30 22:08:08 +0300

    lesson 5 add

--- lesson-05/exercise-03/index.js
+++ lesson-05/exercise-03/index.js
@@ -6,34 +6,14 @@ let tr = document.querySelector('template').content.querySelector('tr');
 var a = 5;
 function insert_Row()
 {
-    // for (let i = 0; i < 3; i++) {
-    //     let tr1 = tr.cloneNode(true);
-    //     // td1.innerHTML = i;
-    //      tr1.querySelectorAll('td')[0].textContent = i;
-    //     tr1.querySelectorAll('td')[1].textContent = 'q';
-    //     table.appendChild(tr1);
-    // }
+    for (let i = 0; i < 3; i++) {
+        let tr1 = tr.cloneNode(true);
+        // td1.innerHTML = i;
+        tr1.querySelectorAll('td')[0].textContent = i;
+        tr1.querySelectorAll('td')[1].textContent = 'q';
+        table.appendChild(tr1);
+    }
 
-    // table.appendChild(tr);
-
-
+    table.appendChild(tr);
 
 }
-//
-// let buttons = document.querySelectorAll('button');
-//
-// function addClickEventListener(button)
-// {
-//     button.addEventListener('click', btnValueConsoleLog);
-// }
-//
-// function btnValueConsoleLog(e)
-// {
-//     console.log(e.target.value);
-//     e.target.removeEventListener('click', btnValueConsoleLog);
-// }
-//
-// for (let i=0; i < buttons.length; i++){
-//     var button = buttons[i];
-//     addClickEventListener(button);
-// }
--- lesson-05/homework/index.js
+++ lesson-05/homework/index.js
@@ -3,56 +3,86 @@
 
 
 const rows = 10,
-      cols = 10,
-      cells = [];
+    cols = 10,
+    cell = {
+        i: 0,
+        j: 0
+    },
+    cells = [];
 
 function createGrid(root)
 {
-    for (let i = 0; i < rows; i++) {
-        cells[i] = [];
-        for (let j = 0; j < cols; j++) {
-            cells[i][j] = '_';
+    let tbl  = document.createElement('table');
+    tbl.style.borderCollapse = 'collapse';
+
+    for(let i = 0; i < rows; i++){
+        let tr = tbl.insertRow();
+        for(let j = 0; j < cols; j++){
+            let td = tr.insertCell();
+            td.style.border = '1px solid black';
+            td.style.width  = '30px';
+            td.style.height = '30px';
         }
     }
-    cells[0][0] = '0';
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            root.innerHTML += `${cells[i][j]} `;
-        }
-        root.innerText += '\n';
-    }
-
-
-    console.log(cells);
+    root.appendChild(tbl);
 
 }
 
 function moveDown() {
-
+    if (cell.i !== 9){
+        table.rows[cell.i].cells[cell.j].classList.toggle('activeCell');
+        table.rows[++cell.i].cells[cell.j].classList.toggle('activeCell');
+    }
 }
 
 function moveLeft() {
-
+    if (cell.j !== 0){
+        table.rows[cell.i].cells[cell.j].classList.toggle('activeCell');
+        table.rows[cell.i].cells[--cell.j].classList.toggle('activeCell');
+    }
 }
 
 function moveRight() {
-
+    if (cell.j !== 9){
+        table.rows[cell.i].cells[cell.j].classList.toggle('activeCell');
+        table.rows[cell.i].cells[++cell.j].classList.toggle('activeCell');
+    }
 }
 
 function moveUp() {
-
+    if (cell.i !== 0){
+        table.rows[cell.i].cells[cell.j].classList.toggle('activeCell');
+        table.rows[--cell.i].cells[cell.j].classList.toggle('activeCell');
+    }
 }
 
 const moves = {
-  '97': moveLeft,
-  '115': moveDown,
-  '119': moveUp,
-  '100': moveRight
+    '1092': moveLeft,
+    '1099': moveDown,
+    '1094': moveUp,
+    '1074': moveRight,
+    '97': moveLeft,
+    '115': moveDown,
+    '119': moveUp,
+    '100': moveRight
 };
 
 const root = document.getElementById('container');
 createGrid(root);
+let table = document.querySelector('#container table');
+table.rows[0].cells[0].classList.toggle('activeCell');
 
 document.body.addEventListener('keypress', (e) => {
-  moves[e.keyCode]();
+    moves[e.keyCode]();
+});
+
+table.addEventListener('mousedown', function (e)
+{
+    e.stopPropagation();
+    e.preventDefault();
+    table.rows[cell.i].cells[cell.j].classList.toggle('activeCell');
+    cell.i = e.target.parentElement.rowIndex;
+    cell.j = e.target.cellIndex;
+    e.target.classList.toggle('activeCell');
+
 });
